98e12120bf9a51cc05a7c4b5b0395b2e3ba5393a
wangpengfei <2601339907@qq.com>
2020-11-23 18:25:44 +0800

    feat: first add

---
+++ server/configure.js
@@ -0,0 +1,33 @@
+var path = require("path");
+var routes = require("./routes");
+var exphbs = require("express-handlebars");
+var express = require("express");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var morgan = require("morgan");
+var methodOverride = require("method-override");
+var errorHandler = require("errorhandler");
+
+module.exports = function(app) {
+    app.use(morgan("dev"));
+    app.use(bodyParser.urlencoded({"extended": true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(cookieParser("some-secret-value-here"));
+    routes(app); //moving the routes to routes folder.
+    app.use("/public/", express.static(path.join(__dirname, "../public")));
+
+    if ("development" === app.get("env")) {
+        app.use(errorHandler());
+    }
+
+    // set view engine
+    app.engine("handlebars", exphbs.create({
+        defaultLayout: "main",
+        layoutsDir: app.get("views") + "/layouts",
+        partialsDir: [app.get("views") + "/partials"]
+    }).engine);
+    app.set("view engine", "handlebars");
+
+    return app;
+};
