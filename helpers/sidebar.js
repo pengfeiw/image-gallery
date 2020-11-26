2ef2a7f895bef9233395f7b69760c690b085ba30
wangpengfei <2601339907@qq.com>
2020-11-26 17:55:34 +0800

    feat: 增加侧边栏功能

--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -1,12 +1,41 @@
-var Stats = require("./stats");
-var Images = require("./images");
-var Comments = require("./comments");
+// var Stats = require("./stats");
+// var Images = require("./images");
+// var Comments = require("./comments");
+
+// module.exports = function (viewModel, callback) {
+//     viewModel.sidebar = {
+//         stats: Stats(),
+//         popular: Images.popular(),
+//         comments: Comments.newest()
+//     };
+//     callback(viewModel);
+// };
+
+
+var Stats = require('./stats');
+var Images = require('./images');
+var Comments = require('./comments');
+var async = require('async');
 
 module.exports = function (viewModel, callback) {
-    viewModel.sidebar = {
-        stats: Stats(),
-        popular: Images.popular(),
-        comments: Comments.newest()
-    };
-    callback(viewModel);
+    async.parallel([
+        function (next) {
+            // next(null, Stats());
+            Stats(next);
+        },
+        function (next) {
+            // next(null, Images.popular());
+            Images.popular(next);
+        },
+        function (next) {
+            Comments.newest(next);
+        }
+    ], function (err, results) {
+        viewModel.sidebar = {
+            stats: results[0],
+            popular: results[1],
+            comments: results[2]
+        };
+        callback(viewModel);
+    });
 };
